cd440ff934c49e48ae4627821cee39ceb865c95b
luca-costa <89141090+MrUnLucks@users.noreply.github.com>
2024-10-04 01:55:35 +0200

    chore: unify chat and testwebsocket

--- frontend/src/components/Chat.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <div class="rounded-lg border-2 border-gray-700 text-emerald-200"></div>
-</template>
--- frontend/src/components/TestWebSocket.vue
+++ frontend/src/components/TestWebSocket.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, onMounted, onUnmounted } from 'vue';
+import { ref, onMounted, onUnmounted } from 'vue';
 import { socket } from '../socket';
 
 const history = ref<string[]>([]);
