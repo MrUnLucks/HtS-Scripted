b7d231976b9248b098f21c6e5d4476aef99e7cd6
luca-costa <89141090+MrUnLucks@users.noreply.github.com>
2024-10-06 21:38:27 +0200

    chore: upload test images

--- listeners/message.ts
+++ listeners/message.ts
@@ -3,8 +3,6 @@ import { ExtendedServer, ExtendedSocket } from '../types/';
 export default {
   name: 'message' as const,
   execute(socket: ExtendedSocket, io: ExtendedServer, message: string) {
-    console.log(socket.data.name);
-
     io.emit('message', { name: socket.data.name, message });
   },
 };
--- lobby/players.ts
+++ lobby/players.ts
@@ -5,6 +5,10 @@ export const addPlayer = (player: { id: string; name: string }) => {
   players[player.id] = { id: player.id, ready: false, name: player.name };
 };
 
+export const removePlayer = (playerId: string) => {
+  delete players[playerId];
+};
+
 export const setPlayerReady = (playerId: string, ready: boolean = true) => {
   const targetPlayer = players[playerId];
   if (!targetPlayer) return;
@@ -19,8 +23,4 @@ export const resetReadyAllPlayers = () => {
 
 export const areAllPlayersReady = (): boolean => {
   return Array.from(Object.values(players)).every((player) => player.ready);
-};
-
-export const removePlayer = (playerId: string) => {
-  delete players[playerId];
 };
